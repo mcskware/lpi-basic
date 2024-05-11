48749d6a291bf78997a781777a710870bf1b7aaa
mcskware <13559364+mcskware@users.noreply.github.com>
2024-05-11 09:58:28 +0200

    Add lib to interpreter

---
+++ lpi-interpreter/src/lib.rs
@@ -0,0 +1,7 @@
+//! Applesoft II BASIC interpreter
+
+/// Interpret the parsed tokens
+#[must_use]
+pub fn interpret() -> String {
+    "Applesoft II BASIC interpreter".to_owned()
+}
--- lpi-interpreter/src/main.rs
+++ lpi-interpreter/src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     println!("Hello, world!");
     println!("Lexer: {}", lpi_lexer::lex());
     println!("Parser: {}", lpi_parser::parse());
+    println!("Interpreter: {}", lpi_interpreter::interpret());
 }
