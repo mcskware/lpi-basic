7ef547df429951f85d61d6f1ca999d195b5aec36
mcskware <13559364+mcskware@users.noreply.github.com>
2024-05-13 10:38:49 +0200

    Handle operator precedence in expression parsing

--- lpi-parser/src/lib.rs
+++ lpi-parser/src/lib.rs
@@ -135,19 +135,25 @@ pub fn parse(tokens: &[String]) -> ParseNode {
     }
 
     // combine arithmetic expressions until there are no more to combine
-    combine_arithmetic_expressions(&mut root);
+    combine_expressions(&mut root);
 
     root
 }
 
+fn combine_expressions(node: &mut ParseNode) {
+    combine_arithmetic_expressions(node, &["*", "/"]);
+    combine_arithmetic_expressions(node, &["+", "-"]);
+    combine_arithmetic_expressions(node, &["^"]);
+}
+
 #[allow(clippy::collapsible_if)]
-fn combine_arithmetic_expressions(node: &mut ParseNode) {
+fn combine_arithmetic_expressions(node: &mut ParseNode, operators: &[&str]) {
     // combine arithmetic expressions
     let mut continue_parsing = true;
     while continue_parsing {
         continue_parsing = false;
         for i in 0..node.children.len() - 1 {
-            if is_arithmetic_operator(&node.children[i]) {
+            if is_arithmetic_operator(&node.children[i], operators) {
                 if i > 0 && i < node.children.len() - 1 {
                     if is_numerical(node.children[i - 1].node_type)
                         && is_numerical(node.children[i + 1].node_type)
@@ -179,13 +185,8 @@ fn is_numerical(node_type: NodeType) -> bool {
         || node_type == NodeType::Expression
 }
 
-fn is_arithmetic_operator(node: &ParseNode) -> bool {
-    node.node_type == NodeType::Symbol
-        && (node.value == "+"
-            || node.value == "-"
-            || node.value == "*"
-            || node.value == "/"
-            || node.value == "^")
+fn is_arithmetic_operator(node: &ParseNode, operators: &[&str]) -> bool {
+    node.node_type == NodeType::Symbol && operators.contains(&node.value.as_str())
 }
 
 #[cfg(test)]
@@ -314,6 +315,75 @@ mod tests {
                             node_type: NodeType::Number,
                             value: "5".to_owned(),
                             children: Vec::new(),
+                        },
+                    ],
+                },
+            ],
+        };
+
+        assert_eq!(parse, expected);
+    }
+
+    #[test]
+    fn test_operator_precedence() {
+        let tokens = vec![
+            "10".to_owned(),
+            "PRINT".to_owned(),
+            "3".to_owned(),
+            "+".to_owned(),
+            "4".to_owned(),
+            "*".to_owned(),
+            "5".to_owned(),
+        ];
+        let parse = parse(&tokens);
+        let expected = ParseNode {
+            node_type: NodeType::Root,
+            value: String::new(),
+            children: vec![
+                ParseNode {
+                    node_type: NodeType::LineNumber,
+                    value: "10".to_owned(),
+                    children: Vec::new(),
+                },
+                ParseNode {
+                    node_type: NodeType::StatementName,
+                    value: "PRINT".to_owned(),
+                    children: Vec::new(),
+                },
+                ParseNode {
+                    node_type: NodeType::Expression,
+                    value: String::new(),
+                    children: vec![
+                        ParseNode {
+                            node_type: NodeType::Number,
+                            value: "3".to_owned(),
+                            children: Vec::new(),
+                        },
+                        ParseNode {
+                            node_type: NodeType::Symbol,
+                            value: "+".to_owned(),
+                            children: Vec::new(),
+                        },
+                        ParseNode {
+                            node_type: NodeType::Expression,
+                            value: String::new(),
+                            children: vec![
+                                ParseNode {
+                                    node_type: NodeType::Number,
+                                    value: "4".to_owned(),
+                                    children: Vec::new(),
+                                },
+                                ParseNode {
+                                    node_type: NodeType::Symbol,
+                                    value: "*".to_owned(),
+                                    children: Vec::new(),
+                                },
+                                ParseNode {
+                                    node_type: NodeType::Number,
+                                    value: "5".to_owned(),
+                                    children: Vec::new(),
+                                },
+                            ],
                         },
                     ],
                 },
