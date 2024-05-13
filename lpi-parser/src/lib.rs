403b0cb783f96bbf61f61b78510568e50d0e77f2
mcskware <13559364+mcskware@users.noreply.github.com>
2024-05-13 10:52:21 +0200

    Parenthesis grouping

--- lpi-parser/src/lib.rs
+++ lpi-parser/src/lib.rs
@@ -141,14 +141,53 @@ pub fn parse(tokens: &[String]) -> ParseNode {
 }
 
 fn combine_expressions(node: &mut ParseNode) {
-    combine_arithmetic_expressions(node, &["*", "/"]);
-    combine_arithmetic_expressions(node, &["+", "-"]);
-    combine_arithmetic_expressions(node, &["^"]);
+    let mut did_something = true;
+    while did_something {
+        did_something = false;
+        did_something |= combine_arithmetic_expressions(node, &["*", "/"]);
+        did_something |= combine_arithmetic_expressions(node, &["+", "-"]);
+        did_something |= combine_arithmetic_expressions(node, &["^"]);
+        did_something |= combine_parens(node);
+    }
+}
+
+fn combine_parens(node: &mut ParseNode) -> bool {
+    // here we want to combine a left paren, a numerical, and a right paren
+    let mut did_something = false;
+    let mut continue_parsing = true;
+    while continue_parsing {
+        continue_parsing = false;
+        for i in 0..node.children.len() - 1 {
+            if node.children[i].node_type == NodeType::Symbol
+                && node.children[i].value == "("
+                && is_numerical(node.children[i + 1].node_type)
+                && node.children[i + 2].node_type == NodeType::Symbol
+                && node.children[i + 2].value == ")"
+            {
+                let left = node.children.remove(i);
+                let num = node.children.remove(i);
+                let right = node.children.remove(i);
+                // combine the expression into a new node with these children
+                let new_node = ParseNode {
+                    node_type: NodeType::Expression,
+                    value: String::new(),
+                    children: vec![left, num, right],
+                };
+                // replace the combined nodes with the new one
+                node.children.insert(i, new_node);
+                continue_parsing = true;
+                did_something = true;
+                break;
+            }
+        }
+    }
+    did_something
 }
 
 #[allow(clippy::collapsible_if)]
-fn combine_arithmetic_expressions(node: &mut ParseNode, operators: &[&str]) {
+fn combine_arithmetic_expressions(node: &mut ParseNode, operators: &[&str]) -> bool {
     // combine arithmetic expressions
+    let mut did_something = false;
     let mut continue_parsing = true;
     while continue_parsing {
         continue_parsing = false;
@@ -170,12 +209,14 @@ fn combine_arithmetic_expressions(node: &mut ParseNode, operators: &[&str]) {
                         // replace the combined nodes with the new one
                         node.children.insert(i - 1, new_node);
                         continue_parsing = true;
+                        did_something = true;
                         break;
                     }
                 }
             }
         }
     }
+    did_something
 }
 
 fn is_numerical(node_type: NodeType) -> bool {
@@ -384,6 +425,93 @@ mod tests {
                                     children: Vec::new(),
                                 },
                             ],
+                        },
+                    ],
+                },
+            ],
+        };
+
+        assert_eq!(parse, expected);
+    }
+
+    #[test]
+    fn test_parens() {
+        let tokens = vec![
+            "10".to_owned(),
+            "PRINT".to_owned(),
+            "(".to_owned(),
+            "3".to_owned(),
+            "+".to_owned(),
+            "4".to_owned(),
+            ")".to_owned(),
+            "*".to_owned(),
+            "5".to_owned(),
+        ];
+        let parse = parse(&tokens);
+        let expected = ParseNode {
+            node_type: NodeType::Root,
+            value: String::new(),
+            children: vec![
+                ParseNode {
+                    node_type: NodeType::LineNumber,
+                    value: "10".to_owned(),
+                    children: Vec::new(),
+                },
+                ParseNode {
+                    node_type: NodeType::StatementName,
+                    value: "PRINT".to_owned(),
+                    children: Vec::new(),
+                },
+                ParseNode {
+                    node_type: NodeType::Expression,
+                    value: String::new(),
+                    children: vec![
+                        ParseNode {
+                            node_type: NodeType::Expression,
+                            value: String::new(),
+                            children: vec![
+                                ParseNode {
+                                    node_type: NodeType::Symbol,
+                                    value: "(".to_owned(),
+                                    children: Vec::new(),
+                                },
+                                ParseNode {
+                                    node_type: NodeType::Expression,
+                                    value: String::new(),
+                                    children: vec![
+                                        ParseNode {
+                                            node_type: NodeType::Number,
+                                            value: "3".to_owned(),
+                                            children: Vec::new(),
+                                        },
+                                        ParseNode {
+                                            node_type: NodeType::Symbol,
+                                            value: "+".to_owned(),
+                                            children: Vec::new(),
+                                        },
+                                        ParseNode {
+                                            node_type: NodeType::Number,
+                                            value: "4".to_owned(),
+                                            children: Vec::new(),
+                                        },
+                                    ],
+                                },
+                                ParseNode {
+                                    node_type: NodeType::Symbol,
+                                    value: ")".to_owned(),
+                                    children: Vec::new(),
+                                },
+                            ],
+                        },
+                        ParseNode {
+                            node_type: NodeType::Symbol,
+                            value: "*".to_owned(),
+                            children: Vec::new(),
+                        },
+                        ParseNode {
+                            node_type: NodeType::Number,
+                            value: "5".to_owned(),
+                            children: Vec::new(),
                         },
                     ],
                 },
